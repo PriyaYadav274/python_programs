71e63cc7c998fed5338e0fb81285c4b9ddaae310
Priya Yadav <priyadav274@gmail.com>
2025-05-07 06:51:26 +0530

    Update and rename WAP to input a sentence from the user & print their word or string in reverse order.py to WAP to input a sentence from the user and print their word or string in reverse order.py

--- WAP to input a sentence from the user and print their word or string in reverse order.py
+++ WAP to input a sentence from the user and print their word or string in reverse order.py
@@ -1,4 +1,4 @@
-# WAP to input a sentence from the user & print their word/string in reverse order.
+# WAP to input a sentence from the user and print their word/string in reverse order.
 print("||Write a word or a string to print them in reverse order.||\n")
 s1 = input("Enter a sentence: ")
 print(s1[::-1])
