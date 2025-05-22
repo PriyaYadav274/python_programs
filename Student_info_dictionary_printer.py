14c1438757504c3aba69c1c62cd44d1840b22287
Priya Yadav <priyadav274@gmail.com>
2025-05-22 10:13:27 +0530

    Add files via upload

---
+++ Student_info_dictionary_printer.py
@@ -0,0 +1,11 @@
+# Creating the dictionary
+
+student_info = {
+    "Name": "Tina",
+    "Age": 19,
+    "Roll_no.": 22,
+}
+
+# Printing the dictionary values
+for key in student_info:
+    print(key, ":", student_info[key])
